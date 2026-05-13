3b13e40728c9488adc8507988ca0baefed6ac459
Panayiotis Halouvas <phalouvas@gmail.com>
2026-05-13 11:29:42 +0300

    Update revision for index.html in service worker

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.2pj660fn5rg"
+    "revision": "0.9qk3bq5ic1"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
